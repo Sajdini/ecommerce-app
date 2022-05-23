3c581c46ca11f4bca10c9f3a0e7b382953b537a7
Sajdini <64154305+Sajdini@users.noreply.github.com>
2022-05-23 18:42:22 +0200

    implemented stripe payment

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,22 +1,9 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
-import { urlFor } from "../lib/client";
+import { urlFor } from '../lib/client';
 
-const FooterBanner = ({
-  footerBanner: {
-    discount,
-    largeText1,
-    largeText2,
-    saleTime,
-    smallText,
-    midText,
-    desc,
-    product,
-    buttonText,
-    image,
-  },
-}) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
@@ -35,10 +22,12 @@ const FooterBanner = ({
           </Link>
         </div>
 
-        <img src={urlFor(image)} className="footer-banner-image" />
+        <img 
+          src={urlFor(image)} className="footer-banner-image"
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FooterBanner;
+export default FooterBanner
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import "../styles/globals.css";
-import { Toaster } from "react-hot-toast";
-import { Layout } from "../components";
-import { StateContext } from "../context/StateContext";
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
@@ -10,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </StateContext>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
